02da13343647493dd4c323c3f10f722ecd51652e
İSMAİL ENES EROĞLU <73061163+firnox61@users.noreply.github.com>
2024-05-04 18:11:47 +0300

    Extension eklemeleri yapıldı

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -63,6 +63,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.ConfigureCustomExceptionMiddleware();
 app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 app.UseHttpsRedirection();
 app.UseAuthentication();
